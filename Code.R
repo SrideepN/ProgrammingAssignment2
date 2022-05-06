f044b4a6ac08c8169b4ed41dc6a81514f6872d16
SrideepN <nsrideep@gmail.com>
2022-05-06 22:33:55 +0530

    The initial code

---
+++ Code.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) i <<- inverse
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+CacheSolve <- function(x, ...) {
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data) %*% data
+  x$setinv(i)
+  i
+}
